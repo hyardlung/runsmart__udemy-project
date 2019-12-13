d71a1160fdba76246c8c8f022320b72d5ed06016
Dmitry_Volkov <hyardlu@protonmail.com>
2019-12-13 08:15:47 +0700

    Слайдер собран на tiny-slider (без адаптации)

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,34 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/catalog__arrow_left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/catalog__arrow_right.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }    
-        ]
-    });
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         speed: 1200,
+//         // adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/catalog__arrow_left.svg"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/catalog__arrow_right.svg"></button>',
+//         responsive: [
+//             {
+//                 breakpoint: 992,
+//                 settings: {
+//                     dots: true,
+//                     arrows: false
+//                 }
+//             }    
+//         ]
+//     });
+//   });
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: falsei
   });
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+});
