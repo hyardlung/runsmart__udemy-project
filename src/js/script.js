f2786629831126235a205f94734c23d6440b48b3
Dmitry Volkov <hyardlu@protonmail.com>
2020-03-28 15:31:18 +0700

    feat: add catalog with tabs; refact: return slick-slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,42 @@
-// $(document).ready(function(){
-//     $('.carousel__inner').slick({
-//         speed: 1200,
-//         // adaptiveHeight: true,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/catalog__arrow_left.svg"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="icons/catalog__arrow_right.svg"></button>',
-//         responsive: [
-//             {
-//                 breakpoint: 992,
-//                 settings: {
-//                     dots: true,
-//                     arrows: false
-//                 }
-//             }    
-//         ]
-//     });
-//   });
+$(document).ready(function() {
+  $(".carousel__inner").slick({
+    speed: 1200,
+    adaptiveHeight: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="../icons/catalog__arrow_left.svg"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="../icons/catalog__arrow_right.svg"></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          dots: true,
+          arrows: false
+        }
+      }
+    ]
+  })
 
-const slider = tns({
-    container: '.carousel__inner',
-    items: 1,
-    slideBy: 'page',
-    autoplay: false,
-    controls: false,
-    nav: falsei
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
   });
-
-document.querySelector('.prev').addEventListener('click', function () {
-    slider.goTo('prev');
 });
 
-document.querySelector('.next').addEventListener('click', function () {
-    slider.goTo('next');
-});
+// const slider = tns({
+//   container: ".carousel__inner",
+//   items: 1,
+//   slideBy: "page",
+//   autoplay: false,
+//   controls: false,
+//   nav: false
+// });
+
+// document.querySelector(".prev").addEventListener("click", function() {
+//   slider.goTo("prev");
+// });
+
+// document.querySelector(".next").addEventListener("click", function() {
+//   slider.goTo("next");
+// });
