385e683778c477bf6e6d65e7afd020a38865df90
Hyardlung <hyardlu@protonmail.com>
2020-04-08 13:32:18 +0700

    fix: path to arrow buttons in carousel

--- src/js/script.js
+++ src/js/script.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
     speed: 1200,
     adaptiveHeight: true,
     prevArrow:
-      '<button type="button" class="slick-prev"><img src="../icons/catalog__arrow_left.svg"></button>',
+      '<button type="button" class="slick-prev"><img src="icons/catalog__arrow_left.svg"></button>',
     nextArrow:
-      '<button type="button" class="slick-next"><img src="../icons/catalog__arrow_right.svg"></button>',
+      '<button type="button" class="slick-next"><img src="icons/catalog__arrow_right.svg"></button>',
     responsive: [
       {
         breakpoint: 992,
