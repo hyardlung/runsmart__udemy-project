bcc3878548610ea7abcd466f2cc5a824e4bcde45
Hyardlung <hyardlu@protonmail.com>
2020-04-09 13:36:36 +0700

    feat: add script that implements page scroll

--- src/js/script.js
+++ src/js/script.js
@@ -126,4 +126,10 @@ $(document).ready(function () {
       $(".page-up").fadeOut();
     }
   });
+
+  $("a[href^='#']").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
 });
