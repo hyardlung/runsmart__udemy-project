212f4d27b34eee6af400ec4f39460bdd3311b5ab
Hyardlung <hyardlu@protonmail.com>
2020-04-11 13:28:53 +0700

    refact: rewrite gulp & prepare project for upload
    Gulp compiled files into the same src directory from where he took.
    I screwed the imagemin plugins (for compressing images) and htmlmin
    (for minifying html, removes all spaces, comments and line breaks)
    to gulpfile.js, and also added a number of tasks that all processed
    files put in the dist directory.
    
    ... but I still don’t understand very well how it all works

---
+++ dist/js/script.js
@@ -0,0 +1,137 @@
+$(document).ready(function () {
+  $(".carousel__inner").slick({
+    speed: 1200,
+    adaptiveHeight: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="icons/catalog__arrow_left.svg"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="icons/catalog__arrow_right.svg"></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          dots: true,
+          arrows: false,
+        },
+      },
+    ],
+  });
+
+  $("ul.catalog__tabs").on(
+    "click",
+    "li:not(.catalog__tab_active)",
+    function () {
+      $(this)
+        .addClass("catalog__tab_active")
+        .siblings()
+        .removeClass("catalog__tab_active")
+        .closest("div.container")
+        .find("div.catalog__content")
+        .removeClass("catalog__content_active")
+        .eq($(this).index())
+        .addClass("catalog__content_active");
+    }
+  );
+
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on("click", function (e) {
+        e.preventDefault();
+        $(".catalog-item__content")
+          .eq(i)
+          .toggleClass("catalog-item__content_active");
+        $(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
+      });
+    });
+  }
+
+  toggleSlide(".catalog-item__link");
+  toggleSlide(".catalog-item__back");
+
+  // Modal
+
+  $("[data-modal=consultation]").on("click", function () {
+    $(".overlay, #consultation").fadeIn("slow");
+  });
+  $(".modal__close").on("click", function () {
+    $(".overlay, #consultation, #order, #thanks").fadeOut("slow");
+  });
+
+  $(".button_mini").each(function (i) {
+    $(this).on("click", function () {
+      $("#order .modal__descr").text($(".catalog-item__subtitle").eq(i).text());
+      $(".overlay, #order").fadeIn("slow");
+    });
+  });
+
+  //Form validation
+  function validateForms(form) {
+    $(form).validate({
+      errorClass: "invalid",
+      errorElement: "div",
+      rules: {
+        name: {
+          required: true,
+          minlength: 2,
+        },
+        phone: "required",
+        email: {
+          required: true,
+          email: true,
+        },
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста, введите ваше имя",
+          minlength: jQuery.validator.format("Не менее {0} символов!"),
+        },
+        phone: "Введите номер телефона",
+        email: {
+          required: "Введите адрес электронной почты",
+          email: "Ведите в формате example@domain.com",
+        },
+      },
+    });
+  }
+
+  validateForms("#consultation-form");
+  validateForms("#consultation form");
+  validateForms("#order form");
+
+  //Маска для номера телефона
+  $("[type=tel]").mask("+7 (000) 000-00-00", {
+    placeholder: "+7 (___) ___-__-__",
+  });
+
+  $("form").submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize(),
+    }).done(function () {
+      $(this).find("input").val("");
+      $("#consultation, #order").fadeOut();
+      $(".overlay, #thanks").fadeIn("slow");
+      $("form").trigger("reset");
+    });
+    return false;
+  });
+
+  //Smooth scroll
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1600) {
+      $(".page-up").fadeIn();
+    } else {
+      $(".page-up").fadeOut();
+    }
+  });
+
+  $("a[href=#up]").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
+
+  new WOW().init();
+});
