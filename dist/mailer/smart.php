6d4931684595e70fbbe02a7bc0bc72d0e3679e8b
Hyardlung <hyardlu@protonmail.com>
2020-04-12 11:36:03 +0700

    fix: change images for suunto m2 & polar ft4

--- dist/mailer/smart.php
+++ dist/mailer/smart.php
@@ -8,15 +8,22 @@ require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
-// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+// $mail->SMTPDebug = 2;                               // Enable verbose debug output
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->isSMTP();                               // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'hyardlung@gmail.com';                 // Наш логин
 $mail->Password = 'EFfsY9ZBBNBJqqbyJVzePW2MtFcj5';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465;                                    // TCP port to connect to
+$mail->Port = 465;    
+// $mail->SMTPOptions = array(
+// 	'ssl' => array(
+// 			'verify_peer' => false,
+// 			'verify_peer_name' => false,
+// 			'allow_self_signed' => true
+// 	)
+// );                                // TCP port to connect to
  
 $mail->setFrom('hyardlung@gmail.com', 'RunSmart');   // От кого письмо 
 $mail->addAddress('hyardlung@yandex.com');     // Add a recipient
